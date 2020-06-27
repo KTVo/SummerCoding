3e8b24eead92213bee2ec38f25d88b78bda534fd
KTVo <foolycooly288622@yahoo.com>
2020-06-26 23:42:27 -0700

    Reviewed multi-referenced ptrs

---
+++ Python_Intro/Python_Syntax/Python_Syntax/Python_Syntax.py
@@ -0,0 +1,38 @@
+"""
+This is a comment block
+"""
+
+#this is a single comment
+
+def funct(x):  
+    if(x != 0):
+        print('Hello x = ', x)
+
+def funct2(a):
+    print('Abigail Hobbs\n')
+    print('I can undo what I said.\n')
+    print('a = ', a)
+
+dictionary = {
+    "one": "Kevin",
+    "cool": 75,
+    "lambert": 12.21
+}
+
+class Car:
+    make = ""
+    year = 0000
+    model = ""
+
+def main():
+    """
+    arr = {"food", "can", "booze", 25, 0.01, 25, 25}
+    num = input('Enter name: ')
+    funct(num)
+    funct2(arr)
+    """
+    print(dictionary["lambert"])
+
+main()
+
+
